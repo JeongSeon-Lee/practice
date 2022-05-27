4aad9cae1a817522051af2d06b52bbdf93aab954
Jeongseon <ljs658794@gmail.com>
2022-05-27 15:41:11 +0900

    혼공 예제 8-3, sizeof 연산자를 사용한 배열

---
+++ honGongE8-3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int score[5];
+    int i;
+    int total = 0;
+    double avg;
+    int count;
+
+    count = sizeof(score) / sizeof(score[0]);  // 배열 요소의 개수 계산
+
+    for (i = 0; i < count; i++) 
+    {
+        scanf("%d", &score[i]);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        total += score[i];
+    }
+
+    avg = total / (double)count;
+
+    for (i = 0; i < count; i++)
+    {
+        printf("%5d", score[i]);
+    }
+
+    printf("\n");
+    
+    printf("평균 : %0.1lf\n", avg);
+
+    return 0;
+}
